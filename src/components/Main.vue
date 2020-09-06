9f2fdb17e592911031f6ad13dde74f8541744fc9
Algot Holton <drawserqzez@gmail.com>
2020-09-06 20:45:48 +0200

    New look and improvements for mobile.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,35 +1,77 @@
 <template>
     <div>
-        <h2>Kunskaper inom programmering</h2>
+        <h2>Min techstack</h2>
         <div class="container">
             <div class="column">
-                <h3>Backend</h3>
-                <ul>
-                    <li>C# & ASP.NET</li>
-                    <li>Node.js Express</li>
-                    <li>Python</li>
-                </ul>
-            </div>
-            <div class="column">
                 <h3>Frontend</h3>
-                <ul>
-                    <li>Vue.js</li>
-                    <li>Bootstrap</li>
-                    <li>html, css & vanilla js</li>
-                </ul>
+                <div :key="item.id" v-for="item in this.frontEnd">
+                    {{ item }}
+                </div>
+            </div>
+            <span class="divider"></span>
+            <div class="column">
+                <h3>Backend</h3>
+                <div :key="item.id" v-for="item in backEnd">
+                    {{ item }}
+                </div>
             </div>
         </div>
     </div>
 </template>
 
+<script>
+export default {
+    Name: 'Main',
+    data: function () {
+        return {
+            frontEnd: [
+                "Vue.JS",
+                "Bootstrap",
+                "HTML",
+                "CSS",
+                "Vanilla JS"
+            ],
+            backEnd: [
+                ".NET C#",
+                "ASP.NET MVC",
+                "Blazor",
+                "Node.js Express",
+                "Python"
+            ]
+        }
+    },
+}
+</script>
+
 <style>
- .container {
-    column-count: 2;
-    column-rule: 1px solid black;
-    /* padding: 12px; */
-    margin-top: 12px;
- }
- .column {
-    margin-top: -20px;
- }
+    .container {
+        display: flex;
+        flex-flow: column nowrap;
+
+        width: 100%;
+        height: 100%;
+    }
+
+    .divider {
+        background-color: black;
+        width: 80%;
+        height: 1px;
+        margin: 1rem 0 1rem 10%;
+    }
+
+    @media (min-width: 786px) {
+        .container {
+            flex-flow: row nowrap;
+        }
+
+        .column {
+            width: 48%;
+            height: 100%;
+        }
+
+        .divider {
+            width: 2%;
+            height: 0;
+        }
+    }
 </style>
